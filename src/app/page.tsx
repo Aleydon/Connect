a3d7accb5da5897e936de3834ffb147d1a0e64cc
Aleydon <aleydonroberto@gmail.com>
2025-02-17 21:33:42 -0300

    ADD: Setup tailwind themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div className="flex justify-center items-center">
-      <h3 className="text-2xl bold">Page</h3>
+      <h3 className="text-2xl bold font-heading">Page</h3>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,27 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
+    colors: {
+      blue: '#5F9DE2',
+      purple: '#9781F3',
+      danger: '#F05D6C',
+
+      gray: {
+        100: '#DAE4F2',
+        200: '#C8D0DA',
+        300: '#95A1B1',
+        400: '#6F7D90',
+        500: '#2A313C',
+        600: '#21252C',
+        700: '#191D24',
+        800: '#13161B',
+        900: '#0F1216'
+      }
+    },
+    fontFamily: {
+      heading: 'var(--font-oxanium)',
+      sans: 'var(--font-montserrat)'
+    },
     extend: {
       colors: {
         background: 'var(--background)',
